6fd53bc6f146e661931b984edfcff8724473fcc7
ojy0533 <ojy0533@naver.com>
2026-01-17 22:04:50 +0900

    환경설정

---
+++ Transfermarkt-Value-Analysis(TVA)/World/world_tva_data.py
@@ -0,0 +1,10 @@
+import requests
+import time
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.rcParams['font.family'] = 'Malgun Gothic'
+matplotlib.rcParams['axes.unicode_minus'] = False
+from bs4 import BeautifulSoup
+
+# 
