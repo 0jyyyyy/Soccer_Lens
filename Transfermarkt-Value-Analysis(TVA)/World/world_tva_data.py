bc2d50394ff8b3b1ea49806dc7237d182a6d9998
ojy0533 <ojy0533@naver.com>
2026-01-17 22:13:12 +0900

    Transfermarkt 연결 확인

--- Transfermarkt-Value-Analysis(TVA)/World/world_tva_data.py
+++ Transfermarkt-Value-Analysis(TVA)/World/world_tva_data.py
@@ -7,4 +7,18 @@ matplotlib.rcParams['font.family'] = 'Malgun Gothic'
 matplotlib.rcParams['axes.unicode_minus'] = False
 from bs4 import BeautifulSoup
 
-# 
+# requests.get() 으로 url 정보 요청하기
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Safari/537.36'}
+number = []
+name = []
+position = []
+age = []
+nationality = []
+team = []
+value = []
+
+for i in range(1, 5):
+  url = f'https://www.transfermarkt.co.kr/spieler-statistik/wertvollstespieler/marktwertetop?ajax=yw1&page={i}'
+  res = requests.get(url, headers=headers)
+
+  print(res.status_code) # 200 잘 나오는 거 확인
