ebc0c3656b575a1206806332b64f8046582f4b67
ojy0533 <ojy0533@naver.com>
2026-01-18 19:16:41 +0900

    bs4로 데이터 크롤링

--- Transfermarkt-Value-Analysis(TVA)/World/world_tva_data.py
+++ Transfermarkt-Value-Analysis(TVA)/World/world_tva_data.py
@@ -17,8 +17,25 @@ nationality = []
 team = []
 value = []
 
-for i in range(1, 5):
+for i in range(1, 21):
   url = f'https://www.transfermarkt.co.kr/spieler-statistik/wertvollstespieler/marktwertetop?ajax=yw1&page={i}'
   res = requests.get(url, headers=headers)
 
-  print(res.status_code) # 200 잘 나오는 거 확인
+  # print(res.status_code) # 200 잘 나오는 거 확인
+  soup = BeautifulSoup(res.content,'html.parser')
+
+  player_list = []
+  player_info = soup.find_all('tr', class_=['odd','even'])
+  for info in player_info:
+    player = info.find_all('td')
+    player[0]
+    number = player[0].text
+    name = player[3].text
+    position = player[4].text
+    age = player[5].text
+    nationality = player[6].img['alt']
+    team = player[7].img['alt']
+    value = player[8].text.strip()
+    value = value[1:-1] # '€숫자m' 일때 €의 인덱스는 [1]이고 m은 [-1]이니 이걸 제외하고 저장하는것이다.
+
+  player_list.append([number,name,position,age,nationality,team,value]) 
